0b37b872349b54a486d585fadbfcd11a43aa903b
ky.luong <luong_cong_ky@yahoo.com>
2026-01-24 12:07:27 +0700

    fix: build issues

--- prisma.config.mjs
+++ prisma.config.mjs
@@ -2,6 +2,10 @@
 import { defineConfig } from '@prisma/config';
 import 'dotenv/config';
 
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL is not defined in the environment. Please check your Vercel Project Settings.');
+}
+
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   datasource: {
